e858132bb73d37827eae5918ba92fdbe6cc30064
Bev <bevinsuryaatmaja@gmail.com>
2025-09-18 10:49:07 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+// =================== CLASS PERSON ===================
+public class Person
+{
+    private string nama; // Encapsulation (data hiding)
+    public string Nama
+    {
+        get { return nama; }
+        set { nama = value; }
+    }
+
+    public Person(string nama) // Constructor
+    {
+        Nama = nama;
+    }
+
+    public virtual void Perkenalan() // virtual -> bisa di override
+    {
+        Console.WriteLine($"Halo, saya {Nama}");
+    }
+}
